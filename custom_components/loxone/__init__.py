223aa4606d6357d4e5bd308f46ed9764dcf4624b
Sharat Koya <sharat.koya@me.com>
2026-02-01 22:00:32 +0000

    fix: update config entry options via async_update_entry

--- custom_components/loxone/__init__.py
+++ custom_components/loxone/__init__.py
@@ -199,13 +199,13 @@ async def async_migrate_entry(hass, config_entry):
     # _LOGGER.debug("Migrating from version %s", config_entry.version)
     if config_entry.version == 1:
         new = {**config_entry.options, CONF_LIGHTCONTROLLER_SUBCONTROLS_GEN: True}
-        config_entry.options = {**new}
+        hass.config_entries.async_update_entry(config_entry, options=new)
         config_entry.version = 2
         _LOGGER.info("Migration to version %s successful", 2)
 
     if config_entry.version == 2:
         new = {**config_entry.options, CONF_SCENE_GEN_DELAY: DEFAULT_DELAY_SCENE}
-        config_entry.options = {**new}
+        hass.config_entries.async_update_entry(config_entry, options=new)
         config_entry.version = 3
         _LOGGER.info("Migration to version %s successful", 3)
     if config_entry.version == 3:
@@ -213,7 +213,7 @@ async def async_migrate_entry(hass, config_entry):
             **config_entry.options,
             CONF_EMIT_RAW_LOXONE_EVENT: False,
         }
-        config_entry.options = {**new}
+        hass.config_entries.async_update_entry(config_entry, options=new)
         config_entry.version = 4
         _LOGGER.info("Migration to version %s successful", 4)
     return True
